8f87502a5f7d07972ac1d4007cbeb1cc910984e2
Sora-210 <sokominto1231@gmail.com>
2023-07-15 08:54:41 +0000

    chore: eslintのルール変更
    anyを利用できるように変更(error->warning)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
+    "@typescript-eslint/no-unsafe-assignment": "warn",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
