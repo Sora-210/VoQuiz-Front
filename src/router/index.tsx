fb7d9d6bad46561613745d3f09540f660c5c3041
Sora-210 <sokominto1231@gmail.com>
2023-07-15 20:00:40 +0000

    add: 投票作成用のルーティングを追加

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { Main } from '../components/pages/Main'
 import { Error404 } from '../components/pages/Error404'
 import { Dev } from '../components/pages/Dev'
+import { CreatePage } from '../components/pages/Create'
 import { VotePage } from '../components/pages/Vote'
 import { QuizPage } from '../components/pages/Quiz'
 
@@ -11,6 +12,10 @@ export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />
+    },
+    {
+        path: "/create",
+        element: <CreatePage />
     },
     {
         path: "/vote/:id",
