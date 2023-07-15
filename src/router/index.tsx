e1acfe91407875a3deefad2180ba9d0da756097b
Sora-210 <sokominto1231@gmail.com>
2023-07-15 11:24:22 +0000

    add: デバックモードの追加
    APIのヘルス状態を確認するため

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom' 
 import { Main } from '../components/pages/Main'
 import { Error404 } from '../components/pages/Error404'
+import { Dev } from '../components/pages/Dev'
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Main />
+    },
+    {
+        path: "/dev",
+        element: <Dev />,
     },
     {
         path: "*",
