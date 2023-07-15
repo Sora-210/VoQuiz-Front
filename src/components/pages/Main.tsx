b3d9002052008f4d257ae648cc92d0fd1e7218b6
Sora-210 <sokominto1231@gmail.com>
2023-07-15 13:39:08 +0000

    refact: トップページの修正
    デザインやロジックの修正

--- src/components/pages/Main.tsx
+++ src/components/pages/Main.tsx
@@ -1,49 +1,44 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Box, Card, TextField, Stack, Button, Typography } from '@mui/material'
 import { DefaultLayout } from '../layouts/DefaultLayout'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, generatePath } from 'react-router-dom'
 
 export const Main:FC = () => {
     const navigate = useNavigate()
-
-    const PushDebugPage = () => {
-        navigate("/dev")
-    }
+    const [voteId, setVoteId] = useState('')
 
     return (
         <>
             <DefaultLayout>
                 <Box maxWidth="100%">
-                    <Card sx={{m: 3, p: 3}}>
-                        <Typography>
+                    <Card variant="outlined" sx={{m: 3, p: 3, borderRadius: "2em" }}>
+                        <Typography variant="h4" sx={{ textAlign:"left", my: 2, fontWeight: 'bold' }}>
                             参加する
                         </Typography>
-                        <Typography>
+                        <Typography sx={{ my: 5 }}>
                             投票コードを入力して投票しよう！
                         </Typography>
                         <Stack spacing={3} sx={{mx: 5, my:2}}>
-                            <TextField id="outlined-basic" label="UserName" variant="outlined" />
-                            <TextField id="outlined-basic" label="VoteID" variant="outlined" />
-                            <Button variant="contained">
+                            <TextField id="outlined-basic" label="VoteID" variant="outlined" inputRef={setVoteId} size="small" />
+                            <Button variant="contained" disabled={!voteId} onClick={() => navigate(generatePath('/vote/:id', {id: voteId.value}))}>
                                 Join
                             </Button>
                         </Stack>
                     </Card>
-                    <Card sx={{m: 3, p: 3}}>
-                        <Typography>
+                    <Card variant="outlined" sx={{m: 3, p: 3, borderRadius: "2em" }}>
+                        <Typography variant="h4" sx={{ textAlign:"left", my: 2, fontWeight: 'bold' }}>
                             作成する
                         </Typography>
-                        <Typography>
+                        <Typography sx={{ my: 5 }}>
                             投票を開始しよう！
                         </Typography>
                         <Stack spacing={3} sx={{mx: 5, my:2}}>
-                            <TextField id="outlined-basic" label="UserName" variant="outlined" />
-                            <Button variant="contained">
+                            <Button variant="contained" onClick={() => navigate('/create')}>
                                 Create
                             </Button>
                         </Stack>
                     </Card>
-                    <Button onClick={PushDebugPage}>
+                    <Button onClick={() => navigate("/dev") }>
                         Debug Page
                     </Button>
                 </Box>
