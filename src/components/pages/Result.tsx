37fb07399c11875ca6be5de5fc78ccef58975853
Sora-210 <sokominto1231@gmail.com>
2023-07-16 04:59:59 +0000

    update: 投票情報の表示パネル機能
    情報をわかりやすく視覚化するため表形式に変更

--- src/components/pages/Result.tsx
+++ src/components/pages/Result.tsx
@@ -1,20 +1,24 @@
-import { FC, useState, useEffect } from 'react'
-import { Box, Card, Stack, Button, Typography, Container } from '@mui/material'
+import { FC, useState, useEffect, SyntheticEvent } from 'react'
+import { Box, Card, Stack, Button, Typography, Container, Divider, Tabs, Tab } from '@mui/material'
+// import { TabContext, TabList, Tab } from '@mui/lab'
 import { PieChart } from'@mui/x-charts'
-import { PendingActions, Person } from '@mui/icons-material'
 import { DefaultLayout } from '../layouts/DefaultLayout'
 import { useParams, useNavigate } from 'react-router-dom'
-import { Vote, Option } from '../../types/Vote'
-import { ResultOption, Result } from '../../types/Result'
+import { Vote } from '../../types/Vote'
+import { User, Result } from '../../types/Result'
 import { ChartData } from '../../types/Chart'
 
 export const ResultPage:FC = () => {
     const params = useParams<{voteId: string}>()
     const navigate = useNavigate()
 
+    const [tab, setTab] = useState('0')
+
     const [question, setQuestion] = useState('')
     const [options, setOptions] = useState<Array<ChartData>>([])
     const [series, setSeries] = useState<Array<{data: Array<ChartData>}>>([])
+
+    const [votedUsers, setVotedUsers] = useState<Array<Array<User>>>([])
 
     useEffect(() => {
         const getData = async(voteId: string) => {
@@ -27,6 +31,7 @@ export const ResultPage:FC = () => {
                 setQuestion(vote.question)
 
                 const data: Array<ChartData> = []
+                const users: Array<Array<User>> = []
                 data1.result.options.forEach((option, index) => {
                     const serie: ChartData = {
                         id: index,
@@ -34,11 +39,12 @@ export const ResultPage:FC = () => {
                         label: option.option
                     }
                     data.push(serie)
+
+                    users.push(option.votedUser.sort((a, b) => {return (a.voteCount < b.voteCount ? 1 : -1)}))
                 })
                 setSeries([{data: data}])
                 setOptions(data)
-
-
+                setVotedUsers(users)
             } catch(e) {
                 console.log(e)
                 navigate('/404')
@@ -67,15 +73,31 @@ export const ResultPage:FC = () => {
                                 <Typography variant="h6" sx={{ textAlign:"left", my: 2, fontWeight: 'bold' }}>
                                     | Options
                                 </Typography>
-                                <Stack spacing={1} sx={{mx: 2}}>
+                                <Box>
+                                    <Divider />
+                                    <Box sx={{display: "flex"}}>
+                                        <Typography width='80%' sx={{ my: 1, fontWeight: 'bold', textAlign: 'center' }}>
+                                            選択肢
+                                        </Typography>
+                                        <Typography width='20%' sx={{ my: 1, fontWeight: 'bold', textAlign: 'center' }}>
+                                            票数
+                                        </Typography>
+                                    </Box>
+                                    <Divider />
                                     {options.map((option, index) => {
                                         return (
-                                            <Typography textAlign='left' key={index}>
-                                                - {option.label}: {option.value}
-                                            </Typography>
+                                            <Box sx={{display: "flex"}} key={`select-${index}`}>
+                                                <Typography width='80%' sx={{ my: 1, textAlign: 'center' }}>
+                                                    {option.label}
+                                                </Typography>
+                                                <Typography width='20%' sx={{ my: 1, textAlign: 'center' }}>
+                                                    {option.value}
+                                                </Typography>
+                                            </Box>
                                         )
                                     })}
-                                </Stack>
+                                    <Divider />
+                                </Box>
                             </Card>
                             {series.length && (
                                 <Card sx={{m: 3, p: 3}}>
@@ -84,6 +106,75 @@ export const ResultPage:FC = () => {
                                         width={350}
                                         height={200}
                                     />
+                                </Card>
+                                
+                            )}
+                            {votedUsers.length && (
+                                <Card sx={{m: 3, p: 3}}>
+                                    <Typography variant="h6" sx={{ textAlign:"left", my: 2, fontWeight: 'bold' }}>
+                                        | Details
+                                    </Typography>
+                                    <Box>
+                                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                                            <Tabs aria-label='select' value={tab}
+                                                onChange={(event: SyntheticEvent, v: string) => {
+                                                    setTab(v)
+                                                    console.log(event)
+                                                }} 
+                                            >
+                                                {options.map((option, index) => {
+                                                    return (
+                                                        <Tab
+                                                            label={option.label}
+                                                            value={index.toString()}
+                                                            key={`tab-${index}`}
+                                                            id={`tab-${index}`}
+                                                            aria-controls={`tabpanel-${index}`}
+                                                        />
+                                                    )
+                                                })}
+                                            </Tabs>
+                                            {votedUsers.map((users, index) => {
+                                                return (
+                                                    <Box
+                                                        role="tabpanel"
+                                                        // hidden={tab === index.toString()}
+                                                        key={`tab-panel-${index}`}
+                                                        id={`tab-${index}`}
+                                                        aria-labelledby={`tabpanel-${index}`}
+                                                    >
+                                                        {tab === index.toString() && (
+                                                            <Box sx={{px: 1, py: 2}}>
+                                                                <Divider />
+                                                                <Box sx={{display: "flex"}}>
+                                                                    <Typography width='80%' sx={{ my: 1, fontWeight: 'bold', textAlign: 'center' }}>
+                                                                        ユーザー名
+                                                                    </Typography>
+                                                                    <Typography width='20%' sx={{ my: 1, fontWeight: 'bold', textAlign: 'center' }}>
+                                                                        票数
+                                                                    </Typography>
+                                                                </Box>
+                                                                <Divider />
+                                                                {users.map((user, userIndex) => {
+                                                                    return (
+                                                                        <Box sx={{display: "flex"}} key={`tabpanel-${index}-${userIndex}`}>
+                                                                            <Typography width='80%' sx={{ my: 1, textAlign: 'center' }}>
+                                                                                {user.userName}
+                                                                            </Typography>
+                                                                            <Typography width='20%' sx={{ my: 1, textAlign: 'center' }}>
+                                                                                {user.voteCount}
+                                                                            </Typography>
+                                                                        </Box>
+                                                                    )
+                                                                })}
+                                                                <Divider />
+                                                            </Box>
+                                                        )}
+                                                    </Box>
+                                                )
+                                            })}
+                                        </Box>
+                                    </Box>
                                 </Card>
                                 
                             )}
