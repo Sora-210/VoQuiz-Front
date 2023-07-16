3e557cdaaa4826badba295536e1a4665b9677cf1
Sora-210 <sokominto1231@gmail.com>
2023-07-16 00:08:56 +0000

    update: クイズ機能のロジックを記述

--- src/components/pages/Quiz.tsx
+++ src/components/pages/Quiz.tsx
@@ -1,48 +1,73 @@
-import { FC, useState, useEffect } from 'react'
-import { Box, Card, Stack, Button, Typography } from '@mui/material'
+import { FC, useState, useEffect, ChangeEvent } from 'react'
+import { Box, Card, Stack, Button, Typography, Container, TextField, Divider, Grid } from '@mui/material'
+import { Stepper, Step, StepLabel } from '@mui/material'
 import { DefaultLayout } from '../layouts/DefaultLayout'
-import { useParams } from 'react-router-dom'
+import { generatePath, useNavigate, useParams } from 'react-router-dom'
 import { Quiz } from '../../types/Quiz'
+import { Close, PanoramaFishEye } from '@mui/icons-material'
 
 export const QuizPage:FC = () => {
     const params = useParams<{voteId: string, optionId: string}>()
+    const navigator = useNavigate()
 
-    const [quizes, setQuizes] = useState(Array<Quiz>)
+    const [quizes, setQuizes] = useState<Array<Quiz>>([])
     const [nowQuiz, setNowQuiz] = useState(0)
 
+    // 内部制御用
+    const [steps, setSteps] = useState<Array<string>>(['Name', 'Result'])
+    const [activeStep, setActiveStep] = useState(0)
+
+    // 回答者記録用
+    const [name, setName] = useState('')
+    const [answers, setAnswers] = useState<Array<number>>([])
+    const [score, setScore] = useState(0)
+
+    // 回答送信
+    const sendVote = async (voteId: string, optionId: string) => {
+        try {
+            let count = 0;
+            quizes.forEach((quiz, index) => {
+                count += (quiz.answerIndex === answers[index]) ? 1 : 0;
+            })
+            setScore(count)
+
+            const res = await fetch(`https://fdyoc3p9e3.execute-api.ap-northeast-1.amazonaws.com/vote/${voteId}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    userName: name,
+                    voteOptionId: optionId,
+                    voteCount: count
+                })
+            })
+
+            console.log('Vote')
+        } catch(e) {
+            alert('Error');
+        }
+    }
 
     useEffect(() => {
-        
-        alert('Error')
-        setQuizes([
-            {
-                question: '問題',
-                options: ['選択肢1', '選択肢2', '選択肢3', '選択肢4'],
-                answer: '選択肢1'
+        const getData = async (optionId: string) => {
+            try {
+                const res = await fetch(`https://fdyoc3p9e3.execute-api.ap-northeast-1.amazonaws.com/quiz/${optionId}`)
+                const data:Array<Quiz> = (await res.json()).quizes
+                
+                const label = []
+                for (let i=1; i <= data.length; i++) { label.push(`Quiz${i}`) }
+                const cp = ['Name', 'Result']
+                cp.splice(1, 0, ...label)
+                setSteps(cp)
+
+                setQuizes(data)
+            } catch(e) {
+                alert('Error');
             }
-        ])
-        console.log(quizes)
+        }
 
-        // const getData = (voteId: string, optionId: string) => {
-        //     setQuizes([
-        //         {
-        //             question: '問題',
-        //             options: ['選択肢1', '選択肢2', '選択肢3', '選択肢4'],
-        //             answer: '選択肢1'
-        //         }
-        //     ])
-        //     console.log(quizes[nowQuiz])
-        //     alert('Error')
-        //     // try {
-        //     //     const res = await fetch(`https://fdyoc3p9e3.execute-api.ap-northeast-1.amazonaws.com/quiz/${voteId}/${optionId}`)
-        //     //     const data:Array<Quiz> = (await res.json()).quizes
-        //     //     setQuizes(data)
-        //     // } catch(e) {
-        //     //     alert('Error');
-        //     // }
-        // }
-
-        // getData(params.voteId, params.optionId)
+        getData(params.optionId)
     }, [])
 
 
@@ -50,29 +75,88 @@ export const QuizPage:FC = () => {
         <>
             <DefaultLayout>
                 <Box maxWidth="100%">
-                    {(quizes.length != 0) ?
-                    <>
-                    <Card sx={{m: 3, p: 3}}>
-                        <Typography>
-                            Quiz { nowQuiz }
-                        </Typography>
-                        <Typography>
-                            { quizes[nowQuiz].question }
-                        </Typography>
-                    </Card>
-                    <Stack spacing={4} sx={{mx: 10}}>
-                        {quizes[nowQuiz].options.map((option, index) => {
-                            return (
-                                <Button variant="outlined" key={index} onClick={() => alert(option)}>
-                                    { option }
-                                </Button>
-                            )
-                        })}
-                    </Stack>
-                    </> :
-                    <>
-                    </>
-                    }
+                    <Stepper activeStep={activeStep} alternativeLabel sx={{my: 2}}>
+                        {steps.map((label) => (
+                            <Step key={label}>
+                                <StepLabel>{label}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                    <Container sx={{my: 3}}>
+                        {activeStep === 0 && (
+                            <Card sx={{m: 3, p: 3}}>
+                                <Stack spacing={4}>
+                                    <Typography>
+                                        名前を入力してください！
+                                    </Typography>
+                                    <TextField id='name' label='回答者名' variant='outlined' size='small' fullWidth value={name}
+                                        onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                                            setName(event.target.value)
+                                        }}
+                                    />
+                                    <Button onClick={() => setActiveStep(1)} disabled={name === ''}>次へ</Button>
+                                </Stack>
+                            </Card>
+                        )}
+                        {(0 < activeStep && activeStep < steps.length - 1) && (
+                            <>
+                            <Card sx={{m: 3, p: 2}}>
+                                <Typography variant='h6' sx={{textAlign: 'left'}}>
+                                    Quiz { activeStep }
+                                </Typography>
+                                <Typography>
+                                    { quizes[activeStep - 1].question }
+                                </Typography>
+                            </Card>
+                            <Stack spacing={4} sx={{mx: 6}}>
+                                {quizes[activeStep - 1].options.map((option, index) => {
+                                    return (
+                                        <Button variant="outlined" key={index} onClick={() => {
+                                            setAnswers([...answers, index])
+                                            if (activeStep === steps.length - 2) {sendVote(params.voteId, params.optionId)}
+                                            setActiveStep(activeStep + 1)
+                                        }}>
+                                            { option }
+                                        </Button>
+                                    )
+                                })}
+                            </Stack>
+                            </>
+                        )}
+                        {(activeStep === steps.length - 1) && (
+                            <Card sx={{m: 3, p: 3}}>
+                                <Stack spacing={4}>
+                                    <Typography variant='h6'>
+                                        - 結果 -
+                                    </Typography>
+                                    <Divider />
+                                    <Grid container>
+                                        {quizes.map((quiz, index) => (
+                                            <>
+                                            {(quiz.answerIndex === answers[index]) ?
+                                                <Grid item xs={4}><PanoramaFishEye /></Grid>:<Grid item xs={4}><Close /></Grid>
+                                            }
+                                            <Grid item xs={8}>Quiz {index + 1}</Grid>
+                                            </>
+                                        ))}
+                                    </Grid>
+                                    <Divider />
+                                    <Typography variant='h6'>
+                                        Score: {score}
+                                    </Typography>
+                                    <Divider />
+                                    <Button onClick={() => {
+                                        navigator(generatePath("/result/:voteId", { voteId: params.voteId }))
+                                    }}>投票結果を見る</Button>
+                                </Stack>
+                                
+                            </Card>
+                        )}
+                    </Container>
+                    
+
+
+                    
                 </Box>
             </DefaultLayout>
         </>
