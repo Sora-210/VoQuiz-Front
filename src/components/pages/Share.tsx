97405498d90b00f05c115a48865ce8af4ba695b4
Sora-210 <sokominto1231@gmail.com>
2023-07-16 05:52:11 +0000

    add: シェアページの作成
    投票作成後にidを確認するため

---
+++ src/components/pages/Share.tsx
@@ -0,0 +1,32 @@
+import { FC, useState, useEffect } from 'react'
+import { Typography, Stack } from '@mui/material'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { useParams } from 'react-router-dom'
+
+export const SharePage:FC = () => {
+    const params = useParams<{id: string}>()
+    const [voteId, setVoteId] = useState('')
+    let isFirst = true
+
+    useEffect(() => {
+        if (isFirst) {
+            setVoteId(params.id)
+            isFirst = false
+        }
+    }, [])
+
+    return (
+        <>
+            <DefaultLayout>
+                <Stack spacing={5}>
+                    <Typography variant="h5" color="black">
+                        問題を作成しました
+                    </Typography>
+                    <Typography variant="h6" color="black">
+                        { voteId }
+                    </Typography>
+                </Stack>
+            </DefaultLayout>
+        </>
+    )
+}
