df30937ba4bba5397c784d671e362cd35c9bc04a
Sora-210 <sokominto1231@gmail.com>
2023-07-16 00:30:53 +0000

    docs: promisに関するルールの弱体化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
