558790fd92ddebbb5d9503e17e592fb791cd40f4
Sora-210 <sokominto1231@gmail.com>
2023-07-16 03:08:13 +0000

    update: 'debug page'の削除および簡易型apiチェッカーの実装
    デバックの簡易化のためページ切り替えではなくMainページ内に変更

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom' 
 import { Main } from '../components/pages/Main'
 import { Error404 } from '../components/pages/Error404'
-import { Dev } from '../components/pages/Dev'
 import { CreatePage } from '../components/pages/Create'
 import { ResultPage } from '../components/pages/Result'
 import { VotePage } from '../components/pages/Vote'
@@ -29,10 +28,6 @@ export const router = createBrowserRouter([
     {
         path: "/quiz/:voteId/:optionId",
         element: <QuizPage />
-    },
-    {
-        path: "/dev",
-        element: <Dev />,
     },
     {
         path: "*",
